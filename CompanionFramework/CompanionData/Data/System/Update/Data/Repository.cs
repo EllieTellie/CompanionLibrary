fbac66a41810ff6cd7876d8d43a498e729fb0073
Rene <rene@huntedcow.com>
2022-01-08 22:17:41 +0000

    Added way to output repository back to json.

--- CompanionFramework/CompanionData/Data/System/Update/Data/Repository.cs
+++ CompanionFramework/CompanionData/Data/System/Update/Data/Repository.cs
@@ -5,6 +5,9 @@ using System;
 
 namespace Companion.Data.System.Update
 {
+	/// <summary>
+	/// Holds the repository information retrieved 
+	/// </summary>
 	public class Repository
 	{
 		public string name;
@@ -23,6 +26,10 @@ namespace Companion.Data.System.Update
 		
 		// implement repository files which seems to just always be an empty array?
 
+		/// <summary>
+		/// Create a repository from the json provided.
+		/// </summary>
+		/// <param name="jsonData">Json data</param>
 		public Repository(JsonData jsonData)
 		{
 			name = jsonData.Get("name").GetString();
@@ -52,6 +59,44 @@ namespace Companion.Data.System.Update
 				FrameworkLogger.Exception(e);
 				return null;
 			}
+		}
+
+		/// <summary>
+		/// Write this object out as a json object.
+		/// </summary>
+		/// <param name="writer">Json Writer to use</param>
+		public void Write(JsonWriter writer)
+		{
+			writer.WriteObjectStart();
+
+			writer.WritePropertyName("name");
+			writer.Write(name);
+			writer.WritePropertyName("description");
+			writer.Write(description);
+			writer.WritePropertyName("battleScribeVersion");
+			writer.Write(battleScribeVersion);
+			writer.WritePropertyName("version");
+			writer.Write(version);
+			writer.WritePropertyName("lastUpdated");
+			writer.Write(lastUpdated);
+			writer.WritePropertyName("lastUpdateDescription");
+			writer.Write(lastUpdateDescription);
+			writer.WritePropertyName("indexUrl");
+			writer.Write(indexUrl);
+			writer.WritePropertyName("repositoryUrl");
+			writer.Write(repositoryUrl);
+			writer.WritePropertyName("githubUrl");
+			writer.Write(githubUrl);
+			writer.WritePropertyName("feedUrl");
+			writer.Write(feedUrl);
+			writer.WritePropertyName("bugTrackerUrl");
+			writer.Write(bugTrackerUrl);
+			writer.WritePropertyName("reportBugUrl");
+			writer.Write(reportBugUrl);
+			writer.WritePropertyName("archived");
+			writer.Write(archived);
+
+			writer.WriteObjectEnd();
 		}
 
 		public override string ToString()
