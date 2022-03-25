79997543c06d8f555a2f6a88ac285b4fb5cccae0
Rene <rene@huntedcow.com>
2022-03-25 21:43:27 +0000

    Added helper to get characteristic count.

---
+++ CompanionFramework/CompanionData/Data/Extensions/ProfileExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Companion.Data.Extensions
+{
+    public static class ProfileExtensions
+    {
+        /// <summary>
+        /// Count the number of characteristics in this profile list.
+        /// </summary>
+        /// <param name="profiles">Profile list</param>
+        /// <returns>Total characteristics count</returns>
+        public static int GetCharacteristicCount(this List<Profile> profiles)
+        {
+            int count = 0;
+            foreach (Profile profile in profiles)
+            {
+                count += profile.characteristics.Count;
+            }
+            return count;
+        }
+    }
+}
